d188b85dec7e024ab758c48e536d8748d0c4e29f
TacosConChelas <adriherandezor@gmail.com>
2024-05-18 01:12:27 -0600

    Se continua con las modificacones para el proyecto de la coneccion de la BD

--- ProyectBD_mysql/src/Main/jFMainPantalla.java
+++ ProyectBD_mysql/src/Main/jFMainPantalla.java
@@ -171,11 +171,7 @@ public class jFMainPantalla extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new jFMainPantalla().setVisible(true);
-            }
-        });
+        
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
